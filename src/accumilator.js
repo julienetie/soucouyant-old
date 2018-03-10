19382be81ace84259f3ad2a81e5099c4e8916813
Julien Etienne <j8@protonmail.ch>
2018-03-10 21:33:39 +0000

    Created addNewState

--- src/accumilator.js
+++ src/accumilator.js
@@ -18,6 +18,8 @@ export const persistenceSettings = options => Object.assign(persistence.options,
 // Adds a new state to the accumilator 
 // May create a new frame to do so.
 const addNewState = (state, identity) => {
+    const currentTimeStamp = Date.now();
+    const mergeFidelity = persistence.options.mergeFidelity;
     // Check unique states and add the state if does not yet exist.
     // Directly reference the existing state.
     const stateAsString = JSON.stringify(state);
@@ -30,16 +32,36 @@ const addNewState = (state, identity) => {
         const hasExistingState = JSON
             .stringify(uniqueState) === stateAsString;
         if (hasExistingState) {
-        	directReference = uniqueState;
-        	stateExist = true;
-        	break;
+            directReference = uniqueState;
+            stateExist = true;
+            break;
         }
     }
 
-    if(stateExist === false){
-		uniqueStateReferences.push(state);
-		directReference = uniqueStateReferences[uniqueStateReferences.length - 1];    	
+    if (stateExist === false) {
+        uniqueStateReferences.push(state);
+        directReference = uniqueStateReferences[uniqueStateReferences.length - 1];
     }
-   
 
+    // Find frame by timestamp
+    const accumilatorLength = accumilator.length;
+    const lastFrame = accumilator[accumilatorLength - 1];
+    const lastFrameTimeStamp = lastFrame[0];
+
+    // If within proximity merge. 
+    const withinMergePeriod = lastFrameTimeStamp + mergeFidelity > currentTimeStamp;
+    if (withinMergePeriod) {
+        // merge to last frame
+        lastFrame.push([identity, directReference]);
+    }else{
+    	// Add new frame.
+    	accumilator.push([
+    		currentTimeStamp,
+    		[
+    			identity,
+    			directReference
+    		]
+    	]);
+    }
 }
+
